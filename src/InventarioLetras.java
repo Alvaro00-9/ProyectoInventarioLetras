260bbea15dda26788e64db813eeb4b9225b5355e
Alvaro00-9 <alvdiaz25@alumnos.utalca.cl>
2026-05-26 09:59:06 -0400

    COMMIT DE EXPLICACIÓN Profesor, debido a errores constantes en mis repositorios anteriores(Proyecto Poo, InventarioLetras), me ví en la obligación de eliminarlos debido a su estructura, que tenían commits repetidos en más de 5 ocaciones, lo cual arruinaba por completo la presentación y estructura de los repositorios, dado que no encontré forma posible de eliminar las duplicaciones, ni siquiera alterando la "línea temporal" de github en el compilador, lo cual era un error sumamente molesta que sincesamente, me quitó la paciencia

---
+++ src/InventarioLetras.java
@@ -0,0 +1,214 @@
+public class InventarioLetras {
+
+    private int [] letras;
+    private int totalCount;
+    private int nonZeroCount; //Cantidad de letras que sean distintas de 0
+
+    //Decidí expresar mejor las ideas dentro del código, de forma más ordenada
+
+    ///Método Auxiliar, para llamarlo sin tener que repetir su escritura en varias partes del texto, cosa había hecho en el prototipo de este código (No guardado)
+
+    private int getIndice(char letra) {
+        letra =  Character.toLowerCase(letra);
+        if (letra < 'a' || letra > 'z') {
+            throw new IllegalArgumentException("Carácter no Válido");
+        }
+        return letra - 'a';
+    } ///Estoy usando esta línea de código porque de otro modo no me permite hacer commits
+
+    ///Método Constructor
+    public InventarioLetras(String data) {
+        letras = new int[26];
+        totalCount = 0;
+        nonZeroCount = 0;
+        /// Recorre el Sttring
+        for (int i = 0; i < data.length(); i++) {
+            char c = Character.toLowerCase(data.charAt(i));
+            /// Verificación de letras Inglesas; En el orden del formato ASCI
+            if  (c >= 'a' && c <= 'z') {
+                int indice = c - 'a';
+
+                /// La letra se cuenta por primera vez
+                if (letras[indice] == 0) {
+                    nonZeroCount++;
+                }
+                letras[indice]++;
+                totalCount++; //commits
+            }
+        }
+    }
+
+    /// ENCRIPTADO CESÁR
+
+    public char encriptarCesar(char letra) {
+        int indice = getIndice(letra);
+        return (char) ((indice +3) % 26 + 'a');
+    }
+
+    /// Desencriptar Cesár
+
+    public char desencriptarCesar(char letra) {
+        int indice = getIndice(letra);
+        // Se agrega el 26 para evitar errores como resultados negativos y tal
+        return (char) ((indice -3 + 26) % 26 + 'a');
+    }
+
+
+    /// ENCRIPTACIÓN
+
+    public String encriptarPalabra(String palabra, int desplazamiento) {
+        String resultado = "";
+        for (int i = 0; i < palabra.length(); i++) {
+            char c = Character.toLowerCase(palabra.charAt(i));
+            if (c >= 'a' && c <= 'z') {
+                // Sé ajusta eel carácterr para que al aplicar el desplazamiento fijo de 3 de encriptarCesar no de resultado incorrecto
+                char caracterAjustado = (char) ((c - 'a' + desplazamiento - 3 + 26) % 26 + 'a');
+                char nueva = encriptarCesar(caracterAjustado);
+                resultado = resultado + nueva;
+
+            } else{
+                resultado = resultado + c;
+            }
+        }
+        return resultado; ///Gran parte de este bloque lo hizo auttomaticamente el Compilador, pero al revisar ví que si complementaba correctamente al código
+    }
+
+    /// DESENCRIPTACIÓN
+    public String desencriptarPalabra(String palabra, int desplazamiento) {
+        String resultado = "";
+        for (int i = 0; i < palabra.length(); i++) {
+            char c = Character.toLowerCase(palabra.charAt(i));
+            if (c >= 'a' && c <= 'z') {
+                //Se añade el 52 para asegurar que el resultado matemático dentro del paréntesis nunca sea un número negativo antes de aplicar el operador de módulo (% 26)
+                char caracterAjustado = (char) ((c - 'a' - desplazamiento + 3 + 52) % 26 + 'a');
+                char nueva = desencriptarCesar(caracterAjustado);
+                resultado = resultado + nueva;
+            }  else{
+                resultado = resultado + c;
+            }
+        }
+        return resultado;
+    }
+
+
+    /// MÉTODO GETTER O  GET
+    public int get(char letra) { //Conteo de una letra
+        return letras[getIndice(letra)];
+    }
+    /// SETTER O SET
+
+    public void set(char letra, int valor) {
+        if (valor < 0) {
+            throw new IllegalArgumentException("Valor Inválido");
+        }
+
+        int indice = getIndice(letra);
+
+        /// Ajuste de totalCount
+        totalCount -= letras[indice];
+        totalCount += valor;
+
+        /// Ajuste de nonZeroCount
+        if (letras[indice] == 0 && valor > 0) {
+            nonZeroCount++;
+        }
+        if (letras[indice] > 0 && valor == 0) {
+            nonZeroCount--;
+        }
+        letras[indice] = valor;
+    }
+
+
+    /// SIze
+
+    public int size() {
+        return totalCount;
+    }
+
+    /// isEmpty. Indica true si no hay letras
+
+    public boolean isEmpty() {
+        return nonZeroCount == 0;
+    }
+
+
+    /// toString. Representación del "Inventario"
+
+    public String toString() {
+
+        String resultado = "[";
+        for (int i = 0; i < 26; i++) {
+            for (int j = 0; j < letras[i]; j++) {
+                resultado += (char)(i + 'a');
+            }
+        }
+
+        resultado += "]";
+
+        return resultado;
+    }
+
+    /// ADD o INVENTARIO LETRAS ADD
+
+
+    public InventarioLetras add(InventarioLetras otro) {
+
+        InventarioLetras nuevo =
+                new InventarioLetras("");
+        for (int i = 0; i < 26; i++) {
+
+            nuevo.letras[i] =
+                    this.letras[i] + otro.letras[i];
+
+            if (nuevo.letras[i] >0) {
+                nuevo.nonZeroCount++;
+            }
+            nuevo.totalCount += nuevo.letras[i];
+        }
+        return nuevo;
+    }
+    /// Amplifies (Int n)
+
+    public InventarioLetras amplifies(int n) {
+        if (n < 0) {
+            throw new IllegalArgumentException("Válor Inválido");
+        }
+
+        InventarioLetras nuevo =
+                new InventarioLetras("");
+        for (int i = 0; i < 26; i++) {
+            nuevo.letras[i] = this.letras[i] * n;
+
+            if (nuevo.letras[i] > 0) {
+                nuevo.nonZeroCount++;
+            }
+            nuevo.totalCount += nuevo.letras[i];
+        }
+        return nuevo;
+    }
+    /// Resta de Inventarios
+
+    public InventarioLetras subtract(InventarioLetras otro) {
+        InventarioLetras nuevo =
+                new InventarioLetras("");
+        for (int i = 0; i < 26; i++) {
+
+            int resultado =
+                    this.letras[i] - otro.letras[i];
+
+            /// Retorno null en caso de ser necesario
+            if (resultado < 0) {
+                return null;
+            }
+            nuevo.letras[i] = resultado;
+
+            if (resultado > 0) {
+                nuevo.nonZeroCount++;
+            }
+            nuevo.totalCount += resultado;
+
+        }
+        return nuevo;
+    }
+
+}
